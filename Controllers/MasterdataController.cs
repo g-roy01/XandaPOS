8da8cdf0e51cef3266854d94c9fb1098da20b25b
Raja Ghosh <work.rajaghosh@gmail.com>
2021-06-15 02:19:40 +0530

    FunctionalitySetup - Image Helper Library Creation

--- Controllers/MasterdataController.cs
+++ Controllers/MasterdataController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using XandaPOS.Business;
 using XandaPOS.Models.MasterdataModel;
-using XandaPOS.ImageHelper;
+using XandaPOS.ClientLibrary.ImageHelper;
 using XandaPOS.Edmx;
 using System.Data.Entity;
 using System.IO;
@@ -396,177 +396,49 @@ namespace XandaPOS.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult _Upload(IEnumerable<HttpPostedFileBase> files)
         {
+            
+            ImageHelperMain _imageHelper = new ImageHelperMain();
+
+            var serverPath = HttpContext.Server.MapPath(_imageHelper.TempFolder);
             if (files == null || !files.Any())
                 return Json(new { success = false, errorMessage = "No file uploaded." });
 
             var file = files.FirstOrDefault();  // get ONE only
-            if (file == null || !IsImage(file))
+            if (file == null || !_imageHelper.IsImage(file))
                 return Json(new { success = false, errorMessage = "File is of wrong format." });
 
             if (file.ContentLength <= 0)
                 return Json(new { success = false, errorMessage = "File cannot be zero length." });
 
-            var webPath = GetTempSavedFilePath(file);
+            var webPath = _imageHelper.GetTempSavedFilePath(file,serverPath);
 
             return Json(new { success = true, fileName = webPath.Replace("\\", "/") }); // success
         }
 
+
         [HttpPost]
         public ActionResult Save(string t, string l, string h, string w, string fileName)
         {
+            //t - Image Margin Top
+            //l - Image Margin Left
+            //h - Image Height
+            //w - Image Width
+
             try
             {
                 ImageHelperMain _imageHelper = new ImageHelperMain();
-                // Get file from temporary folder, ...
-                var fn = Path.Combine(Server.MapPath(_imageHelper.MapTempFolder), Path.GetFileName(fileName));
-
-                // ... get the image, ...
-                var img = new WebImage(fn);
-
-                // ... calculate its new dimensions, ...
-                var height = Convert.ToInt32(h.Replace("-", "").Replace("px", ""));
-                var width = Convert.ToInt32(w.Replace("-", "").Replace("px", ""));
-
-                // ... scale it, ...
-                img.Resize(width, height);
-
-                // ... crop the part the user selected, ...
-                var top = Convert.ToInt32(t.Replace("-", "").Replace("px", ""));
-                var left = Convert.ToInt32(l.Replace("-", "").Replace("px", ""));
-                var bottom = img.Height - top - _imageHelper.AvatarHeight;
-                var right = img.Width - left - _imageHelper.AvatarWidth;
-
-                // ... check for validity of calculations, ...
-                if (bottom < 0 || right < 0)
-                {
-                    // If you reach this point, your avatar sizes in here and in the CSS file are different.
-                    // Check _avatarHeight and _avatarWidth in this file
-                    // and height and width for #preview-pane .preview-container in site.avatar.css
-                    throw new ArgumentException("Definitions of dimensions of the cropping window do not match. Talk to the developer who customized the sample code :)");
-                }
-
-                img.Crop(top, left, bottom, right);
-
-                // ... delete the temporary file,...
-                System.IO.File.Delete(fn);
-
-                // ... and save the new one.
-                var newFileName = Path.Combine(_imageHelper.AvatarPath, Path.GetFileName(fn));
-                var newFileLocation = HttpContext.Server.MapPath(newFileName);
-                if (Directory.Exists(Path.GetDirectoryName(newFileLocation)) == false)
-                {
-                    Directory.CreateDirectory(Path.GetDirectoryName(newFileLocation));
-                }
-
-
-                string var1 = DateTime.Now.ToString("MMddyyyyHHmmss");
-                int var2 = (int)Math.Truncate((DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
-                var randomFileName = string.Concat(var1, var2.ToString());
-
+                var _tempPath = HttpContext.Server.MapPath(_imageHelper.TempFolder);
+                var _destinationPath = HttpContext.Server.MapPath(_imageHelper.AvatarPath);
                 
-                string filePartName = GetImageNameFromFilePath(fileName);
-                newFileLocation = newFileLocation.Replace(filePartName, randomFileName);
+                string serverSavedNewFile = _imageHelper.SaveImageInServer(t, l, h, w, fileName, _tempPath, _destinationPath);
 
-                img.Save(newFileLocation);
-
-                newFileName = newFileName.Replace(filePartName, randomFileName);
-
-                return Json(new { success = true, avatarFileLocation = newFileName });
+                return Json(new { success = true, avatarFileLocation = serverSavedNewFile });
             }
             catch (Exception ex)
             {
                 return Json(new { success = false, errorMessage = "Unable to upload file.\nERRORINFO: " + ex.Message });
             }
         }
-
-        private bool IsImage(HttpPostedFileBase file)
-        {
-            ImageHelperMain _imageHelper = new ImageHelperMain();
-            if (file == null) return false;
-            return file.ContentType.Contains("image") ||
-                _imageHelper.ImageFileExtensions.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
-        }
-
-        private string GetImageNameFromFilePath(string fileName)
-        {
-            ImageHelperMain _imageHelper = new ImageHelperMain();
-            // Define destination
-            var serverPath = HttpContext.Server.MapPath(_imageHelper.TempFolder);
-
-            //string replacementString = _imageHelper.TempFolder + "/";
-            fileName = fileName.Replace(_imageHelper.TempFolder + @"/", "");
-            string newFileName = "";
-            if (fileName.EndsWith(".jpg")) //if .jpg Image
-            {   //This step is imp to make sure the .jpg substring is always at the end
-                newFileName = string.Concat(fileName, " ");
-            }
-            newFileName = newFileName.Replace(".jpg ", "");
-            return newFileName;
-        }
-
-        private string GetTempSavedFilePath(HttpPostedFileBase file)
-        {
-            ImageHelperMain _imageHelper = new ImageHelperMain();
-            // Define destination
-            var serverPath = HttpContext.Server.MapPath(_imageHelper.TempFolder);
-            if (Directory.Exists(serverPath) == false)
-            {
-                Directory.CreateDirectory(serverPath);
-            }
-
-            // Generate unique file name
-            var fileName = Path.GetFileName(file.FileName);
-            fileName = SaveTemporaryAvatarFileImage(file, serverPath, fileName);
-
-            // Clean up old files after every save
-            CleanUpTempFolder(1);
-            return Path.Combine(_imageHelper.TempFolder, fileName);
-        }
-
-        private static string SaveTemporaryAvatarFileImage(HttpPostedFileBase file, string serverPath, string fileName)
-        {
-            ImageHelperMain _imageHelper = new ImageHelperMain();
-            var img = new WebImage(file.InputStream);
-            var ratio = img.Height / (double)img.Width;
-            img.Resize(_imageHelper.AvatarScreenWidth, (int)(_imageHelper.AvatarScreenWidth * ratio));
-
-            var fullFileName = Path.Combine(serverPath, fileName);
-            if (System.IO.File.Exists(fullFileName))
-            {
-                System.IO.File.Delete(fullFileName);
-            }
-
-            img.Save(fullFileName);
-            return Path.GetFileName(img.FileName);
-        }
-
-        private void CleanUpTempFolder(int hoursOld)
-        {
-            try
-            {
-                var currentUtcNow = DateTime.UtcNow;
-                var serverPath = HttpContext.Server.MapPath("/Temp");
-                if (!Directory.Exists(serverPath)) return;
-                var fileEntries = Directory.GetFiles(serverPath);
-                foreach (var fileEntry in fileEntries)
-                {
-                    var fileCreationTime = System.IO.File.GetCreationTimeUtc(fileEntry);
-                    var res = currentUtcNow - fileCreationTime;
-                    if (res.TotalHours > hoursOld)
-                    {
-                        System.IO.File.Delete(fileEntry);
-                    }
-                }
-            }
-            catch
-            {
-                // Deliberately empty.
-            }
-        }
-
-
-
-
 
         #endregion
 
